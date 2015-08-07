df2d8c0e32c57e4245620903b355459e531e7305
zhang bin  <zhangbin@zhangbin-MBA.local>
2015-08-07 19:26:25 +0800

    修复 Factory::dao bug

--- src/Ludo/Support/Factory.php
+++ src/Ludo/Support/Factory.php
@@ -12,11 +12,13 @@ class Factory
     {
         $daoName = trim($name, '/').'Dao';
         $daoName = ucfirst($daoName);
-        $file = LD_DAO_PATH.DIRECTORY_SEPARATOR.$daoName.php;
-        if (file_exists($file)) {
-            return new $daoName;
-        } else {
-            return new \BaseDao($name, $connectionName);
+        $dirs = ClassLoader::getDirectories();
+        foreach ($dirs as $dir) {
+            $filename = $dir.DIRECTORY_SEPARATOR.$daoName.php;
+            if (file_exists($filename)) {
+                return new $daoName;
+            }
         }
+        return new \BaseDao($name, $connectionName);
     }
 }
