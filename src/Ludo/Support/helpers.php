87d796fccfc424f09fc67994e2f5877f4006a743
zhangbin <zhangbin@funtoygame.com>
2020-06-05 17:25:16 +0800

    删除str_contains函数

--- src/Ludo/Support/helpers.php
+++ src/Ludo/Support/helpers.php
@@ -279,23 +279,6 @@ function start_with(string $haystack, $needles)
 }
 
 /**
- * Determine if a given string contains a given substring.
- *
- * @param string $haystack
- * @param string|array $needles
- * @return bool
- */
-function str_contains($haystack, $needles)
-{
-    foreach ((array)$needles as $needle) {
-        if ($needle != '' && strpos($haystack, $needle) !== false) {
-            return true;
-        }
-    }
-    return false;
-}
-
-/**
  * Replace a given value in the string sequentially with an array.
  *
  * @param string $search
