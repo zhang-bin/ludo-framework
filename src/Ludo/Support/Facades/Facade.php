92635f6b184d8bf9ecf6a9291f39488d34767848
zhangbin <zhangbin@funtoygame.com>
2021-01-15 11:40:47 +0800

    支持非static调用

--- src/Ludo/Support/Facades/Facade.php
+++ src/Ludo/Support/Facades/Facade.php
@@ -23,4 +23,20 @@ abstract class Facade implements FacadeInterface
         }
         return $instance->$method(...$args);
     }
+
+    /**
+     * Call facade method
+     *
+     * @param string $method method name
+     * @param array $args method arguments
+     * @return mixed
+     */
+    public function __call(string $method, array $args)
+    {
+        $instance = ServiceProvider::getMainInstance()->getRegisteredAbstract(static::getFacadeAccessor());
+        if (!$instance) {
+            throw new RuntimeException('A facade root has not been set.');
+        }
+        return $instance->$method(...$args);
+    }
 }
